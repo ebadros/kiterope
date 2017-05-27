2870487578873c2ec18d8d190578616f0f3b500b
Eric Badros <ebadros@gmail.com>
2017-05-26 22:55:53 -0700

    Updated requirements.txt

--- kiterope/settings/settingsConfiguration.py
+++ kiterope/settings/settingsConfiguration.py
@@ -3,14 +3,15 @@ ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # You can key the configurations off of anything - I use project path.
 configs = {'/Users/eric/Dropbox/_syncFolder/Business/kiterope/code/kiterope/kiterope/settings': 'development',
-    '/home/ebadros/webapps/kiterope/kiterope/kiterope/settings': 'production',
-    '/home/ebadros/webapps/kiterope_dev/kiterope/kiterope/settings': 'staging',
+    '/opt/python/current/app/kiterope/settings': 'production',
+    '/opt/python/current/app/kiterope/kiterope/settings': 'staging',
 }
 
 # Import the configuration settings file - REPLACE projectname with your project
 #config_module = __import__('kiterope.settings.%s' % configs[ROOT_PATH], globals(), locals(), 'kiterope')
 
 config_module = __import__('kiterope.settings.%s' % configs[ROOT_PATH], globals(), locals(), 'kiterope')
+
 
 # Load the config settings properties into the local scope.
 for setting in dir(config_module):
