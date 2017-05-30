2370335caa8881b1e4da4e544fa94fac1bd1be90
Eric Badros <ebadros@gmail.com>
2017-05-30 10:59:17 -0700

    Updated requirements.txt

---
+++ images/management/commands/createsu.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="kiteropeAdmin").exists():
+            User.objects.create_superuser("kiteropeAdmin", "eric@kiterope.com", "kiteropeAdmin")
--- kiterope/settings/production.py
+++ kiterope/settings/production.py
@@ -1,24 +1,26 @@
 #
 from kiterope.settings.common import *
 
-DEBUG = False
+DEBUG = True
 IN_PRODUCTION = True
 
 DOMAIN_NAME = 'kiterope.com'
 ALLOWED_HOSTS = ['localhost', 'kiterope.com']
 
-STATIC_ROOT ='/home/ebadros/webapps/kiterope_static/'
+#STATIC_ROOT ='/opt/python/current/app/kiterope/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_URL = '/static/'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'kiterope_staging_db',
-         'USER': 'kiterope_admin',
-         'PASSWORD': 'eporetik4',
-         'HOST': '127.0.0.1',
-         'PORT': '5432',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
 ROOT_URLCONF = 'kiterope.urls_production'
 
 #This is the id of the site within the database that we should be using as our base site
