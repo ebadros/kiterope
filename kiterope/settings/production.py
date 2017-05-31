29b92e534e6dc96394e64b57eb307f01477a37c1
Eric Badros <ebadros@gmail.com>
2017-05-30 17:32:54 -0700

    Updated requirements.txt

--- kiterope/settings/production.py
+++ kiterope/settings/production.py
@@ -12,7 +12,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join("/opt/python/ondeck/app/assets"),
+    os.path.join("/opt/python/ondeck/app/assets/"),
 #    '/Users/eric/Dropbox/_syncFolder/Business/kiterope/code/kiterope/static/',
 )
 
