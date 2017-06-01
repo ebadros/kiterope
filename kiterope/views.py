6fa09e7dbf060303a9d4462b9e8521c3be20c18d
Eric Badros <ebadros@gmail.com>
2017-06-01 16:13:40 -0700

    Revising settings to get elasticsearch working with AWS

--- kiterope/views.py
+++ kiterope/views.py
@@ -1161,7 +1161,6 @@ def secret_page(request, *args, **kwargs):
 conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
 
 def sign_s3_upload(request):
-    print("this is the request %s" % request)
     object_name = request.GET['objectName']
     content_type = mimetypes.guess_type(object_name)[0]
 
