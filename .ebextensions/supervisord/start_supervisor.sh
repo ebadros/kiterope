166a8777e35aff1111821892f21661433d3b90b0
Eric Badros <ebadros@gmail.com>
2017-06-05 10:22:54 -0700

    Revising settings to get elasticsearch working with AWS

---
+++ .ebextensions/supervisord/start_supervisor.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+virtualenv -p /usr/bin/python2.7 /tmp/senv
+source /tmp/senv/bin/activate
+sudo pip install supervisor
+sudo /usr/local/bin/supervisord -c
+/opt/python/current/app/kiterope/supervisord.conf
+supervisorctl -c /opt/python/current/app/kiterope/supervisord.conf status
