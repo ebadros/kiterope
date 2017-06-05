894a578e4018df3ea7d83b798c76127fc53c8158
Eric Badros <ebadros@gmail.com>
2017-06-05 16:11:03 -0700

    Revising settings to get elasticsearch working with AWS

--- kiterope/supervisord/start_supervisor.sh
+++ kiterope/supervisord/start_supervisor.sh
@@ -2,6 +2,5 @@
 virtualenv -p /usr/bin/python2.7 /tmp/senv
 source /tmp/senv/bin/activate
 sudo pip install supervisor
-sudo /usr/local/bin/supervisord -c
-/opt/python/current/app/kiterope/supervisord.conf
-supervisorctl -c /opt/python/current/app/kiterope/supervisord.conf status
+sudo /usr/local/bin/supervisord -c /opt/python/current/app/kiterope/supervisord/supervisord.conf
+supervisorctl -c /opt/python/current/app/kiterope/supervisord/supervisord.conf status
