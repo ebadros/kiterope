4b3fc57c0ba3b613f470b1dd6845374dea2b06d5
Eric Badros <ebadros@gmail.com>
2017-06-05 18:23:36 -0700

    Revising settings to get elasticsearch working with AWS

--- kiterope/asgi.py
+++ kiterope/asgi.py
@@ -1,5 +1,5 @@
 import os
 import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kiterope.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kiterope.settings.settingsConfiguration")
 channel_layer = channels.asgi.get_channel_layer()
