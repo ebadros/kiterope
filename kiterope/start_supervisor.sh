92790ffce268a69735b914b915891499ea6ac123
Eric Badros <ebadros@gmail.com>
2017-06-09 17:54:08 -0700

    Updating so that Redux

--- kiterope/start_supervisor.sh
+++ kiterope/start_supervisor.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 virtualenv -p /usr/bin/python2.7 /tmp/senv
+rm -f /tmp/supervisor.sock 
 source /tmp/senv/bin/activate
 sudo pip install supervisor
 sudo /usr/local/bin/supervisord -c /opt/python/current/app/kiterope/supervisord.conf
