00946577305bcc498474296510e8869128af9c2f
Eric Badros <ebadros@gmail.com>
2017-06-27 12:37:48 -0700

    Updating so that Redux

--- assets/js/splash.jsx
+++ assets/js/splash.jsx
@@ -31,7 +31,6 @@ export class SplashPage extends React.Component {
     }
 
     checkIfUser() {
-        console.log("check if user")
         var theUrl = 'api/users/i/'
         $.ajax({
             method: 'GET',
