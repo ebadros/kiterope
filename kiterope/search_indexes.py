5a0dc8168d3afb5e9dc98dd918a8fc79a3b93e65
Eric Badros <ebadros@gmail.com>
2017-08-25 15:43:43 -0700

    Migrations added

--- kiterope/search_indexes.py
+++ kiterope/search_indexes.py
@@ -44,9 +44,10 @@ class ProgramIndex(indexes.SearchIndex, indexes.Indexable):
         return Program
 
     def index_queryset(self, using=None):
+
         """Used when the entire index for model is updated."""
 
         #return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
 
-        return self.get_model().objects.all()
+        return self.get_model().objects.filter(viewableBy='ANYONE')
 
