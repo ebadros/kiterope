f8461d9331253a3ab7af6f51f780b2a34611d8f8
Eric Badros <ebadros@gmail.com>
2018-08-09 15:52:39 -0700

    Changing celery setup

--- kiterope/celeryconfig.py
+++ kiterope/celeryconfig.py
@@ -1,5 +1,5 @@
-broker = 'amqp://5ebe2294ecd0e0f08eab7690e2a6ee69:eac8d74fae134a9bbedb21ff824605ead6d858ef@localhost:5672'
-backend = 'rpc://localhost'
+broker_url = 'amqp://5ebe2294ecd0e0f08eab7690e2a6ee69:eac8d74fae134a9bbedb21ff824605ead6d858ef@localhost:5672'
+result_backend = 'rpc://localhost'
 
 task_serializer = 'json'
 result_serializer = 'json'
